372af131add6463022fce270a9034e1db7c6e769
B00884825 <113470732+B00884825@users.noreply.github.com>
2022-09-20 14:53:26 -0300

    Add files via upload
    fizzbuzz

---
+++ desktop.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+for k in range (1,101):
+    if (k%3==0 and k%5==0):
+        print("FizzBuzz")
+    elif(k%3==0):
+        print("Fizz")
+    elif(k%5==0):
+        print("Buzz")
+    else:
+        print(k)
+
+
+# In[ ]:
+
+
+
+
